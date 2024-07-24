4fc09406a9f355bcd51269016933ffceb4b40e73
Tracy Joyner <tracyjoyner444@gmail.com>
2024-07-23 19:52:31 -0500

    added filter buttons

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,1 +1,21 @@
-console.log("hey there!")
+/* fetch jobs */
+const galleryContainer = document.querySelector('.gallery');
+
+fetch('http://localhost:5678/api/works')
+.then(response => {
+return response.json();
+})
+
+gallery.innerHTML = `<figure></figure>`
+
+/* <figure>
+				<img src="assets/images/abajour-tahina.png" alt="Tahina Lampshade">
+				<figcaption>Tahina Lampshade</figcaption>
+			</figure> */
+
+
+/* fetch categories */
+fetch('http://localhost:5678/api/categories')
+.then(response => {
+return response.json();
+})
