c91ed424642ec747be3d90daaffcf0043f254978
Tracy Joyner <tracyjoyner444@gmail.com>
2024-08-03 18:57:34 -0500

    still working on filters

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -47,53 +47,51 @@ function createFilterButton(category) {
 
   button.setAttribute("class", "button");
   button.setAttribute("id", "filter-button");
-  // FIXME use data- prefix attribute for categor.id
   button.setAttribute("data-filter-type", category.id);
+  button.setAttribute("data-category", category.name);
   button.textContent = category.name;
 
   return button;
+}
+
+/**
+ * inserts All button
+ *
+ * @param {object} category - all button
+ * @returns All button
+ */
+function createAllButton() {
+  const allButton = document.createElement("button");
+
+  allButton.setAttribute("class", "button");
+  allButton.setAttribute("id", "all-button");
+  allButton.textContent = "All";
+
+  return allButton;
 }
 
 /** fetch categories and event listeners for filters */
 fetch("http://localhost:5678/api/categories")
   .then((response) => response.json())
   .then((categories) => {
-    // FIXME insert all button here not in HTML(also add event listener for all button)
+    const allButton = createAllButton();
+    filterButtons.appendChild(allButton);
+    allButton.addEventListener("click", () => {
+      galleryContainer.innerHTML = "";
+// TODO: add jobs from cache
+      console.log("all button clicked");
+    });
     categories.forEach((category) => {
       const button = createFilterButton(category);
       filterButtons.appendChild(button);
-      // FIXME add event listener to button variable
+      // FIXME get jobs (using cache)
+      // FIXME filter jobCache by category clicked (get category.id from event target)
+      // FIXME re-insert job cards after removing old ones
+      const filterButton = document.getElementById("filter-button");
+      filterButton.addEventListener("click", () => {
+        galleryContainer.innerHTML = "";
+
+        console.log("filter button clicked");
+      });
     });
   });
-
-// TODO move this to fixme above
-const allButton = document.getElementById("all-button");
-allButton.addEventListener("click", () => {
-  console.log("all button clicked");
-  // FIXME see filter button event listener fixmes
-  const figures = document.querySelectorAll(".job");
-  figures.forEach((figure) => {
-    figure.style.display = "block";
-  });
-});
-// TODO move this to fixme above
-const filterButton = document.getElementById("filter-button");
-filterButton.addEventListener("click", (event) => {
-  console.log("filter button clicked");
-  // FIXME get jobs (using cache)
-  // FIXME filter jobCache by category clicked (get category.id from event target)
-  // FIXME re-insert job cards after removing old ones
-  event.preventDefault();
-  const figures = document.querySelectorAll(".job");
-  const buttonId = document.querySelectorAll("button.filter-type");
-  const figureId = document.querySelectorAll("figure.category-id");
-  figures.forEach((figure) => {
-    if (buttonId === figureId) {
-      figure.style.display = "block";
-    } else {
-      figure.style.display = "none";
-    }
-  });
-});
-
-/*  */
