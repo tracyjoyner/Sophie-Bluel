44a084874a67da4b53d4b19c6d0dbbcb888a8061
Tracy Joyner <tracyjoyner444@gmail.com>
2024-08-06 17:39:18 -0500

    added js folder

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,13 @@
+
+// TODO get login by id and add event listener to login button
+//  TODO in event listener function get the email and password from form fields
+// NOTE - do $event.preventdefault so page doesn't refresh
+// TODO using swagger, test logging in a user both with good and bad credentials
+// TODO use fetch API to send post request to log user in
+// TODO handle response for valid credentials by redirecting to homepage
+// TODO handle response for invalid credentials by showin error message on login page
+
+
+/* login */
+
+/*window.location.assign("index.html");*/
---
+++ FrontEnd/js/script.js
@@ -0,0 +1,108 @@
+/* fetch jobs - create figures for gallery */
+const galleryContainer = document.querySelector(".gallery");
+let jobCache;
+
+/**
+ * inserts job cards in page
+ *
+ * @param {object} job - gallery projects
+ * @returns a job card
+ */
+function createJobFigure(job) {
+  const figure = document.createElement("figure");
+  const figureImg = document.createElement("img");
+  const figureCaption = document.createElement("figcaption");
+  figureImg.setAttribute("src", job.imageUrl);
+  figureImg.setAttribute("alt", job.title);
+  figureCaption.textContent = job.title;
+
+  figure.appendChild(figureImg);
+  figure.appendChild(figureCaption);
+
+  return figure;
+}
+
+/** fetch jobs */
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((jobs) => {
+    jobCache = jobs;
+    jobCache.forEach((job) => {
+      const figure = createJobFigure(job);
+      galleryContainer.appendChild(figure);
+    });
+  });
+
+/* fetch categories - create filter buttons */
+const filterButtons = document.querySelector(".filter-buttons");
+
+/**
+ * inserts category buttons in page
+ *
+ * @param {object} category - gallery job categories
+ * @returns a category button
+ */
+function createFilterButton(category) {
+  const button = document.createElement("button");
+
+  button.setAttribute("class", "button");
+  button.setAttribute("id", "filter-button");
+  button.setAttribute("value", category.id);
+  button.setAttribute("category", category.name);
+  button.textContent = category.name;
+
+  return button;
+}
+
+/**
+ * inserts All button
+ *
+ * @param {object} category - all button
+ * @returns All button
+ */
+function createAllButton() {
+  const allButton = document.createElement("button");
+
+  allButton.setAttribute("class", "button");
+  allButton.setAttribute("id", "all-button");
+  allButton.textContent = "All";
+
+  return allButton;
+}
+
+const allButton = createAllButton();
+filterButtons.appendChild(allButton);
+allButton.addEventListener("click", () => {
+  galleryContainer.innerHTML = "";
+  jobCache.forEach((job) => {
+    const figure = createJobFigure(job);
+    galleryContainer.appendChild(figure);
+  });
+});
+
+/** fetch categories and event listeners for filters */
+fetch("http://localhost:5678/api/categories")
+  .then((response) => response.json())
+  .then((categories) => {
+    categories.forEach((category) => {
+      const filterButton = createFilterButton(category);
+      filterButtons.appendChild(filterButton);
+      filterButton.addEventListener("click", filterJob);
+    });
+  });
+
+function filterJob($event) {
+  const categoryId = parseInt($event.target.value);
+  galleryContainer.innerHTML = "";
+
+  function categoryValue(job) {
+    return categoryId === job.categoryId;
+  }
+  const filteredJobs = jobCache.filter(categoryValue);
+  filteredJobs.forEach((job) => {
+    const figure = createJobFigure(job);
+    galleryContainer.appendChild(figure);
+  });
+}
+
+
